befe10b6a9590bbef2595a299dbacb3948137f2c
Andrew Gardner <Andygardnerucla@gmail.com>
2019-05-12 23:32:25 -0700

    B-7 Selected, attempting BAND # input

--- test_creep.rb
+++ test_creep.rb
@@ -1,6 +1,11 @@
 require 'rubygems'
 require 'selenium-webdriver'
- 
+require 'pp'
+require 'rspec/expectations'
+
+puts "Enter BAND Number:"
+bandNum = gets.chomp
+
 browser = Selenium::WebDriver.for :chrome
 browser.get "https://iims.navercorp.com/login?targetUrl=https://iims.navercorp.com/"
  
@@ -26,7 +31,7 @@ form.send_keys("Ihavethepower1!")
 if button = browser.find_element(:xpath, "//*[@id='login-btn']")
     puts "Found *******"
     button = browser.find_element(:xpath, "//*[@id='login-btn']").click
-    sleep(2)
+    sleep(1)
 else
     puts "Nope ____"
 end
@@ -36,7 +41,7 @@ end
 if card = browser.find_element(:xpath, "//*[@id='card-view-search-area']/li")
     puts "Card link found.\n"
     card = browser.find_element(:xpath, "//*[@id='card-view-search-area']/li").click
-    sleep(3)
+    sleep(1)
     puts "================================="
     puts "Main menu of BAND backend reached.\n"
     puts "================================="
@@ -44,17 +49,84 @@ else
     puts "Card link not found for clickable 'square' card before Main Menu screen."
 end
 
-sleep(1)
+# FIND THE LINK TO STATISTICS PAGE, ACCES STATISTICS PAGE
+sleep(3)
 if statistics = browser.find_element(:xpath, "//*[@id='carousel']/div[1]/ul/li[8]/div/a")
     puts "Card link found.\n"
     card = browser.find_element(:xpath, "//*[@id='carousel']/div[1]/ul/li[8]/div/a").click
-    sleep(3)
+    sleep(1)
     puts "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
     puts "Statistics menu of BAND backend reached.\n"
     puts "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
 else
     puts "Statistcs link not found."
 end
+
+
+browser.switch_to.frame("svc-iframe")
+
+
+# STORING ALL FUNCTION OPTIONS AND SELECTING B7
+#### Interact with the drop down box
+select_list = wait.until {
+    if element = browser.find_element(:xpath, "//*[@id='templateNo']")
+        element = browser.find_element(:xpath, "//*[@id='templateNo']")
+    end
+}
+puts "select_list"
+pp select_list
+ 
+#### Extract all options from the select box
+options=select_list.find_elements(:tag_name => "option")
+
+# Reference URL http://elementalselenium.com/tips/5-select-from-a-dropdown
+dropdown = browser.find_element(id: 'templateNo')
+    select_list = Selenium::WebDriver::Support::Select.new(dropdown)
+    select_list.select_by(:value, '45')
+    puts "Dropdown option selected:"
+    puts selected_option = select_list.selected_options[0].text
+    puts ""
+    sleep(2)
+
+
+# USING BAND PARAMETERS FROM COMMANDLINE GETS AT START OF CREEPER (ARRAY?)
+form = wait.until {
+    if element = browser.find_element(:xpath, "//*[@id='input']")
+        puts "Inputing BAND name into data field."
+        element = browser.find_element(:name, "//*[@id='input']")
+    else
+        puts "BAND data not entered into text field."
+    end
+
+    element if element.displayed?
+}
+sleep(1)
+form.send_keys("75037032")
+sleep(1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
