d4c6516624c67a3a4073b12624a9f102a4fa3c83
Andrew Gardner <Andygardnerucla@gmail.com>
2019-05-18 01:56:43 -0700

    refresh & find cell again working,  not

--- downloadFile.rb
+++ downloadFile.rb
@@ -15,7 +15,7 @@ require 'open-uri'
 "--------------- BELOW THIS IS OLD DELETABLE CODE FOR USE IN TESTING MODULE ONLY ---------------"
 "-----------------------------------------------------------------------------------------------"
 
-bandNum = "75131431"
+bandNum = "75308330"
 
 browser = Selenium::WebDriver.for :chrome
 browser.get "https://iims.navercorp.com/login?targetUrl=https://iims.navercorp.com/"
@@ -54,6 +54,7 @@ else
     puts "No form 'Submit' login-btn found."
 end
 
+sleep(2)
 card = wait.until {
     element = browser.find_element(:xpath, "//*[@id='card-view-search-area']/li")
 }
@@ -62,9 +63,9 @@ card = wait.until {
 if card
     card.click
     puts "Card link found.\n"
-    puts "======================================================="
-    puts "\tMain menu of BAND backend reached.\n"
-    puts "======================================================="
+    "BANNER TEXT FILES WILL NEED TO BE INCLUDED WITH THE EXECUTABLE FOR WHEN OTHER COMPUTERS USE"
+    bannerMainMenu = File.read("./backendBanner.txt")
+    puts bannerMainMenu
 else
     puts "Card link not found for clickable 'square' card before Main Menu screen."
 end
@@ -87,7 +88,7 @@ end
 # FIND THE LINK TO STATISTICS PAGE, ACCES STATISTICS PAGE
 sleep(4)
 if statistics = browser.find_element(:xpath, "//*[@id='carousel']/div[1]/ul/li[8]/div/a")
-    puts "Statistics link found.\n"
+    puts "\tStatistics link found.\n"
     card = browser.find_element(:xpath, "//*[@id='carousel']/div[1]/ul/li[8]/div/a").click
     sleep(2)
     puts "% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %"
@@ -106,7 +107,23 @@ sleep(1)
 "-----------------------------------------------------------------------------------------------"
 "--------------- ABOVE THIS IS OLD DELETABLE CODE FOR USE IN TESTING MODULE ONLY ---------------"
 "-----------------------------------------------------------------------------------------------"
-
+# FUNCTION TO ITERATE BACK TO CELL WITH "RUNNING/SUCEED" TEXT IN ORDER TO BREAK WHILE LOOP OF 'STILL RUNNING'
+def iterate_to_cell(table, bandNum)
+    table.find_elements(:tag_name, "tr").each do |r|  
+        if r.text.include? "CAZ0412"
+            if r.text.include? "#{bandNum}"
+                i = 0
+                r.find_elements(:tag_name, "td").each do |cell|
+                    i += 1
+                    if i == 2
+                        puts "Inside iterate_to_cell, cell.text == #{cell.text}"
+                        return cell.text
+                    end
+                end
+            end
+        end
+    end
+end
 
 
 "THESE WILL NEED TO BE IMPLEMENTED INTO THE THE LOOP BELOW IN DOWNLOADING ALONG WITH 'WAIT UNTIL EXCEL LINK EXISTS'"
@@ -129,12 +146,14 @@ table = wait.until {
 # puts "Table with :tag_name=:\n" + table.text
  "========="
 
- sleep(3)
- # ITERATE THROUGH TABLE ROWS, THEN TABLE CELLS THEN STORE HREF LINKS OF DOWNLOAD LINK INTO ARRAY files_href
- files_href = []
- table.find_elements(:tag_name, "tr").each do |r|  "MAKE ANDY a gets.chomp variable at beginning of program (First and last name)"
-     # PRINTS ALL CELL VALUES (1 CELL PER LINE)
-    # puts "Cell Value: " + r.text
+
+
+
+sleep(3)
+# ITERATE THROUGH TABLE ROWS, THEN TABLE CELLS THEN STORE HREF LINKS OF DOWNLOAD LINK INTO ARRAY files_href
+$cellReturnText = ""
+files_href = []
+table.find_elements(:tag_name, "tr").each do |r|  
     if r.text.include? "CAZ0412"
         if r.text.include? "#{bandNum}"
             puts "***********************************************"
@@ -142,9 +161,7 @@ table = wait.until {
             puts "***********************************************"
             # puts "Row text:"
             # puts r.text
-            # next if cell.text != "Excel"
-            # puts cell.methods
-            # next if cell.text != "Excel"
+    
             i = 0
             r.find_elements(:tag_name, "td").each do |cell|
                 i += 1
@@ -153,61 +170,53 @@ table = wait.until {
                 puts "__________"
                 if i == 2 # THIS REACHES THE DOWNLOAD LINK CELL IN THE TABLE
                     while cell.text == 'RUNNING'
+                        $cellReturnText = iterate_to_cell(table, bandNum)
+                        puts "$cellReturnText: #{$cellReturnText}"
+                        if $cellReturnText == 'SUCCEED'
+                           break
+                        end
+                        #"elapsedTime = Figure out timer ++++++ "
                         puts "STILL RUNNING..."
                         sleep(10) # change this to 30 or 60
                         reloadButton = browser.find_element(:id, "refresh").click
+                        # GRABS NEW REFERENCE TO TABLE AFTER REFRESH
+                        table = wait.until {
+                            element = browser.find_element(:tag_name, "table")
+                        }
+                        puts "After Reload, table:"
+                        puts table
+                        puts "---------------------------------------------------------"
+                        sleep(5)
                         redo
                     end
-                    if cell.text == 'SUCCEED'
-                        cell.find_elements(:tag_name, "a").each do |n|
-                            files_href << n.attribute("href")
-                        end
+                end
+            end
+                puts "above if"
+                puts "$cellReturnText == #{$cellReturnText}"
+                if $cellReturnText == 'SUCCEED'
+                    puts "in if"
+                    cell.find_elements(:tag_name, "a").each do |n|
+                        puts "n.attribute(\"href\") storing into array files_href" 
+                        files_href << n.attribute("href")
                     end
                 end
-                #if cell.find_element(:tag_name, "span")
-                #  puts "span"
-                #end
-                # cell.find_elements(:tag_name, "a").each do |n|
-                #  files_href << n.attribute("href")
-                # end
             end
-        end
+        
     end
  end
 
 
+ p files_href
+
+#files_href.each do |i|
+ #   browser.navigate().to("#{i}")
+    # might need the the brower.switch_to.frame("svc-iframe") here to make it work with each link to get back to main page.
+#end
+#"NAVIGATE GOES TO URI WHICH DOWNLOADS EXCEL FILE"
+##browser.navigate().to("http://dev-stats.admin.band.us/extractResult/downloadResult.nhn?no=18617&filetype=xlsx")
+#"SWITCH BACK TO FRAME TO CONTINUE SEARCHING TABLE"
+##browser.switch_to.frame("svc-iframe")
+    
 
 
-
-
- 
-=begin
-sleep(3)
-# ITERATE THROUGH TABLE ROWS, THEN TABLE CELLS THEN STORE HREF LINKS OF DOWNLOAD LINK INTO ARRAY files_href
-files_href = []
-table.find_elements(:tag_name, "tr").each do |r|  "MAKE ANDY a gets.chomp variable at beginning of program (First and last name)"
-    # PRINTS ALL CELL VALUES (1 CELL PER LINE)
-    # puts "Cell Value: " + r.text
-    if r.text.include? "CAZ0412" && "#{bandNum}"
-        puts "*******************************************"
-        puts "\t Empl. # cell found in table."
-        puts "*******************************************"
-        puts "Row text:"
-        puts r.text
-        r.find_elements(:tag_name, "td").each do |cell|
-            # next if cell.text != "Excel"
-            # puts cell.methods
-            # next if cell.text != "Excel"
-            next if cell.find_element(:tag_name, "span") 
-            cell.find_elements(:tag_name, "a").each do |n|
-                files_href << n.attribute("href")
-            end
-        end        
-    end
-end
-=end
-
-
-
-p files_href
 
