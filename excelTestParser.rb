45fa790211227722c4b3e1b32737753060d940a6
Andrew Gardner <Andygardnerucla@gmail.com>
2019-05-22 11:43:15 -0700

    value[0] and value[7] comparing

--- excelTestParser.rb
+++ excelTestParser.rb
@@ -6,7 +6,7 @@ require 'rubyXL/convenience_methods'
 require 'rubygems'
 require 'awesome_print'
 
-"\n\n"
+puts "\n\n"
 print "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n"
 print "                                            WELCOME TO Parser!                                          \n"
 print "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n"
@@ -20,8 +20,6 @@ until gets.strip() == "exit"
         :start_date => "\nEnter starting date of event with Day first and Month second, followed by year.\n(ex: for June 21, 2019 enter: 21/6/2019)\n",
         :total_days => "\nEnter number of days for the event. (ex: 4)", :band_num => "\nEnter BAND number:"}
 
-    #puts stringsHash.map {|x| x.values[0]}
-    #stringsHash[0] = gets.strip
     puts stringsHash[:event_name]
     stringsHash[:event_name] = gets.strip
     puts stringsHash[:start_date]
@@ -40,23 +38,40 @@ until gets.strip() == "exit"
     workbook = RubyXL::Parser.parse("18579.xlsx")
     puts workbook.worksheets[0] # Returns first worksheet
     # DEFINES WORKBOOK AS WORKSHEET (DONT DELETE)
-    puts worksheet = workbook[0]
-    puts worksheet.sheet_data[0] # Returns first row of the worksheet
-    puts worksheet[0]
-    puts worksheet.sheet_data[1][0]
-    puts worksheet.sheet_data[1][7]
+    worksheet = workbook[0]
+  
     # i = 1 TO SKIP TOP ROW OF TITLES IN SPREADSHEET
     i = 1
+    # SAVE ROWCOUNT FOR MATH OF TOTAL MEMBERS - NEW COUNT
     rowCount = worksheet.sheet_data.rows.size
-    #pp worksheet.inspect
-    ap worksheet.inspect
-    while i < rowCount 
-        if worksheet.sheet_data[i][0] != worksheet.sheet_data[i][7] || worksheet.data[i][7] == nil
-            worksheet.delete_row(i)
-            puts "Not new user. Row ##{i} Deleted."
+    puts "rowCount: #{rowCount}"
+    p worksheet.sheet_data[1][0].value
+    p worksheet.sheet_data[1][7].value
+#    pp worksheet
+
+#  CLEANS USERS THAT DIDN'T JOIN FROM THIS SPECIFIC EVENT
+    index = 0
+    while index < worksheet.sheet_data.rows.size
+        p "#{worksheet.sheet_data.rows.size} - #{index}"
+        firstCell = worksheet.sheet_data[index][0].value
+        lastCell = worksheet.sheet_data[index][7].value
+        p "firstCell = #{firstCell} lastCell = #{lastCell}"
+        if firstCell != lastCell
+            p "DELETE"
+            worksheet.delete_row(index)
+            # puts "Not new user. Row #a#{i} Deleted."
+        else
+            index += 1
         end
-        i += 1
     end
+
+    
+    newCount = index
+    puts "NewCount = :#{newCount}"
+    
+    
+
+
             
     workbook.write("new.xlsx")
 
