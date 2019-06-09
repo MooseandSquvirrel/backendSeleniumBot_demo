74196f963bd17547d24534cfdba3c3cb159218e3
Andrew Gardner <Andygardnerucla@gmail.com>
2019-06-08 22:18:38 -0700

    b7.1 submit/alert both clicked, already started b3

--- excelTestParser.rb
+++ excelTestParser.rb
@@ -479,7 +479,7 @@ def b7_1(eventBandNumsArray, bandsLength)
             sleep(2)
 
         # USING BAND PARAMETERS FROM COMMANDLINE GETS AT START OF CREEPER (ARRAY?)
-        form = $_wait.until {
+        $_form = $_wait.until {
             if element = $_browser.find_element(:tag_name, "textarea")
                 puts "Inputing BAND name into data field."
                 element = $_browser.find_element(:tag_name, "textarea")
@@ -489,7 +489,7 @@ def b7_1(eventBandNumsArray, bandsLength)
             element if element.displayed?
         }
         until bandNameCounter == bandsLength
-            form.send_keys("#{bandsNumArray[bandNameCounter]}\n")
+            $_form.send_keys("#{bandsNumArray[bandNameCounter]}\n")
             bandNameCounter += 1
         end
         sleep(1)
@@ -497,10 +497,99 @@ def b7_1(eventBandNumsArray, bandsLength)
         puts "Good"
 end
   
+def clickit()
+    $_form = $_wait.until {
+        element = $_browser.find_element(:id, "execute")
+        element if element.displayed?
+    }
+    $_form.click
+    sleep(5)
+    puts "Execute Clicked"
+end
+
+def alert_clickit()
+    # alert = $_browser.switch_to.alert
+    $_form = $_wait.until {
+        alert = $_browser.switch_to.alert
+    }
+    $_form.accept
+    sleep(5)
+    puts "Alert Clicked"
+end
 
 
+"@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+"                                  Function Entering Bands for First B3                                  "
+"@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 
+def b3(eventBandNumsArray, bandsLength)
+    bandsNumArray = eventBandNumsArray
 
+    # GETTING ALL BAND NUMBS WITH .collect (ALREADY PERFORMED ABOVE TO GET THE eventNumsArray ((JUST STORED THAT ARRAY INTO bandsNumArray)))
+    # bandsNumArray = bandsArray.collect {|x| x.bandNum}
+
+    bandNameCounter = 0
+    # until bandNameCounter == bandsLength
+        bandsNumArray[bandNameCounter]
+        # STORING ALL FUNCTION OPTIONS AND SELECTING B7
+        # INTERACTING WITH DROPDOWN BOX
+        select_list = $_wait.until {
+            if element = $_browser.find_element(:xpath, "//*[@id='templateNo']")
+                element = $_browser.find_element(:xpath, "//*[@id='templateNo']")
+            end
+        }
+        puts "Function collected from dropdown function options. (into select_list):"
+        pp select_list
+        
+        # EXTRACTING ALL OPTIONS FROM THE DROPDOWN BOX
+        options=select_list.find_elements(:tag_name => "option")
+
+        # REFERENCE URL http://elementalselenium.com/tips/5-select-from-a-dropdown
+        dropdown = $_browser.find_element(id: 'templateNo') #### Comma needed between id: and 'templateNo' ?
+            select_list = Selenium::WebDriver::Support::Select.new(dropdown)
+            select_list.select_by(:value, '45')
+            puts "Dropdown option selected:"
+            puts selected_option = select_list.selected_options[0].text
+            sleep(2)
+
+        # USING BAND PARAMETERS FROM COMMANDLINE GETS AT START OF CREEPER (ARRAY?)
+        $_form = $_wait.until {
+            if element = $_browser.find_element(:tag_name, "textarea")
+                puts "Inputing BAND name into data field."
+                element = $_browser.find_element(:tag_name, "textarea")
+            else
+                puts "BAND data not entered into text field."
+            end
+            element if element.displayed?
+        }
+        until bandNameCounter == bandsLength
+            $_form.send_keys("#{bandsNumArray[bandNameCounter]}\n")
+            bandNameCounter += 1
+        end
+        sleep(1)
+
+        puts "Good"
+end
+  
+def clickit()
+    $_form = $_wait.until {
+        element = $_browser.find_element(:id, "execute")
+        element if element.displayed?
+    }
+    $_form.click
+    sleep(5)
+    puts "Execute Clicked"
+end
+
+def alert_clickit()
+    # alert = $_browser.switch_to.alert
+    $_form = $_wait.until {
+        alert = $_browser.switch_to.alert
+    }
+    $_form.accept
+    sleep(5)
+    puts "Alert Clicked"
+end
 
     def RUN
         userName()
@@ -533,6 +622,7 @@ end
         ap bandsArray[0].campDates
         "----------------------------------------------------------------------------------------------"
     
+        "MAKE THIS SECTION A FUNCTION? WITH ARGUEMENTS BEING PASSED IN THAT ARE REQUIRED"
         # CALLING eventNumsArray FUNCTION TO EXTRACT FROM bandsArray ALL EVENT NAMES INTO ARRAY eventNumsArray
         eventBandNumsArray = Array.new
         eventBandNumsArray = eventNumsArray(bandsArray)
@@ -545,26 +635,20 @@ end
         ap eventBandNumsArray
     
         loadingMessage()
+
+        "-------------------- b7_1 -----------------------" # MAKE THIS A FUNCTION?
         navigate($_userNameVar)
         b7_1(eventBandNumsArray, bandsLength)
+        clickit()
+        alert_clickit()
+        "-------------------- b3 -------------------------" # MAKE THIS A FUNCTION?
+        "MIGHT NEED TO navigate TO iFrame FOR THIS TO BE ABLE TO WORK"
+        
+
     end
     RUN()
     puts "successful run"
     
-    # return    
-    
-
-=begin
-    "FIND CLICK SUBMIT"
-    form = $_wait.until {
-        element = $_browser.find_element(:id, "execute")
-        element if element.displayed?
-    }
-
-    form.click
-
-    # bandNameCounter -= 1
-# end
 
 return 
 
