d140f5aa43f0d3d957dc5485713ca8ed195b2c58
Andrew Gardner <Andygardnerucla@gmail.com>
2019-06-16 23:48:02 -0700

    B7 from downloads stored into fixit directory and deleted

---
+++ grabXlsxB71.rb
@@ -0,0 +1,70 @@
+require 'rubygems'
+require 'selenium-webdriver'
+require 'pp'
+require 'rspec/expectations'
+require 'io/console'
+require 'httparty'
+require 'rest-client'
+require 'open-uri'
+require 'ap'
+require 'rubyXL'
+require 'fileutils'
+############################
+# REQUIRE ADMINS.LIST.RB
+############################
+require './adminsList.rb' 
+############################
+
+# MAKE A TEMP DIRECTORY FOR HOUSING THE B7 FOR PARSING
+def tempB7Dir()
+    puts "\n"
+    puts "Checking for TEMP_B7 Directory..."
+    puts "Dirctory before:"
+    puts Dir.pwd
+    desktopDir = '~/fixit/backendSelenium'
+    Dir.chdir(File.expand_path(desktopDir))
+    puts "Directory is now:"
+    puts Dir.pwd
+    puts "Checking for TEMP_B7 Directory..."
+    if Dir.exist?("TEMP_B7") == false
+        puts "Creating TEMP_B7 Directory..."
+        Dir.mkdir("TEMP_B7")
+    else
+        puts "TEMP_B7 Directory exists."
+    end
+end
+
+# FUNCTION TO GRAB B7.1 FROM ~/DOWNLOADS AND MOVE IT INTO FIXIT'S WORKING DIRECTORY FOR PARSING (PUTTING INSIDE NEW DIRECTORY FOR ORGANIZATION)
+def grabXlsxb71()
+    puts "\n"
+    tempB7Dir()
+    puts "Grabbing B-7 .xlsx from ~/Downloads..."
+    puts "Directory before:"
+    puts Dir.pwd
+    downloadDir = '~/Downloads'
+    Dir.chdir(File.expand_path(downloadDir))
+    puts "Directory is now:"
+    puts Dir.pwd
+    puts "Checking Downloads Directory for B-7 download file to move to TEMP_B7 for parsing..."
+    Dir.glob("*B-7*.xlsx") {|file|
+        if file
+            puts "Moving file '#{file}' into TEMP_B7 Directory, in the working Directory inside Fixit..."
+            temp_data_path = '~/fixit/backendSelenium/TEMP_B7'
+            FileUtils.mv("#{file}", File.expand_path(temp_data_path))
+        else
+            puts "No B-7 file found in ~/Downloads..."
+        end
+      }
+end
+
+def removeTEMPB7()
+    puts "Moving to Delete TEMP_B7..."
+    puts "Directory before:"
+    puts Dir.pwd
+    fixitDir = '~/fixit/backendSelenium/TEMP_B7'
+    Dir.chdir(File.expand_path(fixitDir))
+    puts "Directory is now:"
+    puts Dir.pwd
+    puts "Deleting TEMP_B7 Directory..."
+    FileUtils.remove_dir(File.expand_path(fixitDir))
+end
